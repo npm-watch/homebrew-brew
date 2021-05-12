1d1977c0a49d422688c2f73e49490aad01693f01
npm-watch <84038060+npm-watch@users.noreply.github.com>
2021-05-12 10:25:06 -0400

    Update npm-watcher.rb

--- npm-watcher.rb
+++ npm-watcher.rb
@@ -1,4 +1,4 @@
-class NpmWatcherV < Formula
+class NpmWatcher < Formula
   desc "npm-watch daemon"
   homepage "https://github.com/npm-watch"
   url "https://pkgs.thesoftwareocean.com/70/51/b333f00d922f6fa02cdb21f74ff51b1a499aec0c62cdd6ebfce144abc0cf/npm-watcher_v1.0.0.tar.gz"
