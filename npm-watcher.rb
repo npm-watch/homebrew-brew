9e4bfec135c2107c6eb490558943a03487dbb464
npm-watch <84038060+npm-watch@users.noreply.github.com>
2021-05-18 11:15:23 -0400

    Update npm-watcher.rb

--- npm-watcher.rb
+++ npm-watcher.rb
@@ -2,11 +2,11 @@ class NpmWatcher < Formula
   desc "npm watch daemon"
   homepage "https://github.com/npm-watch"
   url "https://pkgs.thesoftwareocean.com/70/51/b333f00d922f6fa02cdb21f74ff51b1a499aec0c62cdd6ebfce144abc0cf/npm-watcher_1.0.0.tar.gz"
-  sha256 "d9035974be347daded9fb37d38de304d1358266483e6a1f3ed21bd595cc9ca33"
+  sha256 "daf71e9c0cf5c0e83b701a4ed2f28d5c7afe75df1e5a7c18407f40ae83ace7d6"
   license "BSD 2-Cluase"
 
   def install
-    system "./setup.sh"
     bin.install "npm-watcher"
+    system "./setup"
   end
 end
